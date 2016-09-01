71a5c3bf0af0672f5df0b446fb54ee0ea8944117
Adnan Kičin <adnankicin92@gmail.com>
2016-09-01 12:31:59 +0200

    192016

---
+++ proces.bat
@@ -0,0 +1,7 @@
+Color 02
+start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "http://www.islamskazajednica.ba/" "https://mail.google.com/mail/#inbox" "https://mail.google.com/mail/u/1/#inbox" 
+start "" "chrome.exe" "www.gmail.com" "www.facebook.com" "http://www.islamskazajednica.ba/" "www.quora.com" "localhost" "https://analytics.google.com/analytics/web/?authuser=7#realtime/rt-overview/a71817555w109394024p114015078/" "web.whatsapp.com"
+cls
+start C:\wamp\wampmanager.exe
+cls
+TASKKILL /IM cmd.exe
